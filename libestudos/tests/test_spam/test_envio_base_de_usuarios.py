922fd1b25b1987aafe63f2eb208a43c9eb80f361
Daniel Lima <daniel.ngd@gmail.com>
2021-07-03 19:29:14 -0300

    Alterado codigo de produção para enviar emails.

--- libestudos/tests/test_spam/test_envio_base_de_usuarios.py
+++ libestudos/tests/test_spam/test_envio_base_de_usuarios.py
@@ -1,11 +1,30 @@
+import pytest
+
 from libestudos.spam.enviador_de_email import Enviador
 from libestudos.spam.main import EnviadorDeSpam
+from libestudos.spam.models import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Daniel', email='daniel.ngd@gmail.com'),
+            Usuario(nome='Lima', email='daniel_xp_@hotmail.com')
+        ],
+        [
+            Usuario(nome='Daniel', email='daniel.ngd@gmail.com'),
+        ]
+    ]
+)
+def test_qde_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'daniel.ngd@mail.com',
         'Curso PyTools',
         'Excelente curso para operar com ferramentas Python'
     )
+    assert len(usuarios) == enviador.qtd_emails_enviados
--- libestudos/tests/test_spam/test_usuarios.py
+++ libestudos/tests/test_spam/test_usuarios.py
@@ -2,13 +2,16 @@ from libestudos.spam.models import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Daniel')
+    usuario = Usuario(nome='Daniel', email='daniel.ngd@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Daniel'), Usuario(nome='Lima')]
+    usuarios = [
+        Usuario(nome='Daniel', email='daniel.ngd@gmail.com'),
+        Usuario(nome='Lima', email='daniel_xp_@hotmail.com')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()

2f13cc36aceaedfb5306212f0bd61348a7baa4da
Daniel Lima <daniel.ngd@gmail.com>
2021-07-03 21:18:04 -0300

    Adicionado método Mock

--- libestudos/tests/test_spam/test_envio_base_de_usuarios.py
+++ libestudos/tests/test_spam/test_envio_base_de_usuarios.py
@@ -1,20 +1,8 @@
 import pytest
+from unittest.mock import Mock
 
-from libestudos.spam.enviador_de_email import Enviador
 from libestudos.spam.main import EnviadorDeSpam
 from libestudos.spam.models import Usuario
-
-
-class EnviadorMock(Enviador):
-
-    def __init__(self):
-        super().__init__()
-        self.qtd_email_enviados = 0
-        self.parametros_de_envio = None
-
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.qtd_email_enviados += 1
 
 
 @pytest.mark.parametrize(
@@ -32,27 +20,27 @@ class EnviadorMock(Enviador):
 def test_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'daniel.ngd@mail.com',
         'Curso PyTools',
         'Excelente curso para operar com ferramentas Python'
     )
-    assert len(usuarios) == enviador.qtd_email_enviados
+    assert len(usuarios) == enviador.enviar.call_count
 
 
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='Daniel', email='daniel.ngd@gmail.com')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'daniel_xp_@hotmail.com',
         'Curso PyTools',
         'Excelente curso para operar com ferramentas Python'
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with(
         'daniel_xp_@hotmail.com',
         'daniel.ngd@gmail.com',
         'Curso PyTools',

6c1eb1a0fb16f6de170712964cf0680af2ad84b2
Daniel Lima <daniel.ngd@gmail.com>
2021-07-04 14:08:25 -0300

    Adicionado pytest-mock

--- libestudos/tests/test_spam/test_github_api.py
+++ libestudos/tests/test_spam/test_github_api.py
@@ -4,17 +4,16 @@ from libestudos import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/62218061?v=4'
     resp_mock.json.return_value = {
         'login': 'danielngd', 'id': 62218061,
         'avatar_url': url,
     }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('libestudos.github_api.requests.get')
+    get_mock.return_value=resp_mock
+    return url
 
 
 def test_buscar_avatar(avatar_url):

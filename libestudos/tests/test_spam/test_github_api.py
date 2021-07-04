085b57b6b7e600832f7faa471261746cdc52f82e
Daniel Lima <daniel.ngd@gmail.com>
2021-07-04 14:00:46 -0300

    Isolando teste integrado com fixture

--- libestudos/tests/test_spam/test_github_api.py
+++ libestudos/tests/test_spam/test_github_api.py
@@ -1,13 +1,27 @@
+import pytest
 from unittest.mock import Mock
 from libestudos import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/62218061?v=4'
     resp_mock.json.return_value = {
         'login': 'danielngd', 'id': 62218061,
-        'avatar_url': 'https://avatars.githubusercontent.com/u/62218061?v=4',
+        'avatar_url': url,
     }
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('danielngd')
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('danielngd')
     assert 'https://avatars.githubusercontent.com/u/62218061?v=4' == url
